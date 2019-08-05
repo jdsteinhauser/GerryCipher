e03d60c3b2a81f6bd4edf139ede68ea7e15c56e4
Jason Steinhauser <jason.steinhauser@gmail.com>
2019-08-04 20:07:40 -0500

    Version 0.1.0

--- mix.exs
+++ mix.exs
@@ -9,9 +9,18 @@ defmodule GerryCipher.MixProject do
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/jdsteinhauser/gerry_cipher"}
+      package: package(),
     ]
+  end
+
+  defp package do
+  [
+    name: :gerry_cipher,
+    files: ["lib", "mix.exs", "README*", "LICENSE*"],
+    maintainers: ["jason.steinhauser@gmail.com"],
+    licenses: ["MIT"],
+    links: %{"GitHub" => "https://github.com/jdsteinhauser/gerry_cipher"}
+  ]
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -26,8 +35,7 @@ defmodule GerryCipher.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
